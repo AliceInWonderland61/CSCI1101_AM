12b72549b87645ae21979f6cb214496c4ade03aa
Alissen Moreno <40866797+AliceInWonderland61@users.noreply.github.com>
2026-02-18 17:54:00 +0000

    Completed the assignment in class

--- 1101_2026/in-class-examples.py
+++ 1101_2026/in-class-examples.py
@@ -1,25 +1,33 @@
 # Example 1: Create a variable and assign a value
-
+name="Alissen"
 
 # Example 2: Create another variable with a number
-
+num=36
 
 # Example 3: Print text on the screen
-
+print("Hello there, i'm printing text on the screen")
 
 # Example 4: Print what the variable 'name' has inside
-
+print(name)
 
 # Example 5: Print a sentence using the variable 'name'
-
+print("my name is ", name)
 
 # Example 6: Change the value of the variable and print it again
-
+name="Aly"
+print("My nickname is", name)
 # Example 7: Ask user what their favorite animal is
-
-#Example 8: Use the variable create in Example 1
+print("What is your favorite animal?")
+animal=input()
+print("I didn't know that", animal, "was your favorite animal!")
+#Example 8: Use the variable create in Example 2
 #If the variable is greater than 10 print a message 
 #Otherwise, print another message 
+if num>10:
+    print("Congrats, your number is greater than 10!")
+else:
+    print("Sorry, your number is not greater than 10")
+
 
 
 
